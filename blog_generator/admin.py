5b5d9b08396ebf4d20bd291ab4a328f6e80eddf4
Sairaj Jadhav <jadhavsairaj25@gmail.com>
2026-03-13 16:12:26 +0530

    feat(admin): register BlogPost model in admin panel

--- blog_generator/admin.py
+++ blog_generator/admin.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from blog_generator.models import BlogPost
 
+
 @admin.register(BlogPost)
 class BlogPostadmin(admin.ModelAdmin):
-    readonly_fileds = [field.name for field in BlogPost._meta.fields ]
+    readonly_fileds = [field.name for field in BlogPost._meta.fields]
 
     def has_add_permission(self, request):
         return False
